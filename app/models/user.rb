eee50a405f0c931a129a796d6b43f6dfcfb0cf7c
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-23 18:22:02 +0900

    ユーザーモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nickname
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'が無効です 全角文字を入力してください' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'が無効です 全角文字を入力してください' }
+  end
 end
