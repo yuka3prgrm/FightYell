002439a8f5739813b0d4a583a98a967484fd6ad7
yuka3prgrm <yuka3prgrm@gmail.com>
2023-06-29 18:16:36 +0900

    fightの新規投稿

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -7,5 +7,17 @@ class FightsController < ApplicationController
   end
 
   def create
+    @fight = Fight.new(fight_params)
+    if @fight.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def fight_params
+    params.require(:fight).permit(:title,:gool,:unit_id).merge(user_id: current_user.id)
   end
 end
